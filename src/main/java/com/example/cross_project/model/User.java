f782351769f80b82dc53acfb3e3ec64304775d53
almaz-proga <almaz.g05@gmail.com>
2025-09-28 00:46:41 +0300

    Лаб 2: добавил БД

--- src/main/java/com/example/cross_project/model/User.java
+++ src/main/java/com/example/cross_project/model/User.java
@@ -1,8 +1,19 @@
 package com.example.cross_project.model;
 
 import java.util.Set;
-import org.springframework.cglib.core.Local;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,11 +21,26 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Size(min = 2, max = 50)
+    @Column(nullable = false, unique = true, length = 50)
     private String username;
+
+    @NotBlank
+    @Column(nullable = false)
     private String password;
-    private boolean enabled;
+
+    @Column(nullable = false)
+    private boolean enabled = true;
+
+    @OneToMany(cascade = CascadeType.ALL)
     private Set<Role> roles;
 
 
