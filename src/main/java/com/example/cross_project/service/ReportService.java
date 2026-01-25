d1b67a7b1eeb248436d123403ec56ccd67ef28d1
almaz-proga <almaz.g05@gmail.com>
2026-01-25 19:16:41 +0300

    Lab10: Reports fix -edit ReportService -add videoReport -add DataBase Schemas

--- src/main/java/com/example/cross_project/service/ReportService.java
+++ src/main/java/com/example/cross_project/service/ReportService.java
@@ -1,6 +1,7 @@
 package com.example.cross_project.service;
 
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.jxls.transform.poi.JxlsPoiTemplateFillerBuilder;
 
 import com.example.cross_project.repository.AlertRepository;
+import com.example.cross_project.dto.AlertReportDTO;
 import com.example.cross_project.dto.AlertResponse;
 import com.example.cross_project.mapper.AlertMapper;
 
@@ -19,17 +21,30 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class ReportService {
-    private final AlertRepository alertRepository;
+    private final AlertService alertService;
 
     @Value("${report.template-location}")
     private Resource templateLocation;
 
     public ByteArrayResource generateAlertReport() {
-        List<AlertResponse> alerts = alertRepository.findAll().stream()
-            .map(AlertMapper::alertToResponseDTO)
+        
+        List<AlertResponse> alerts = alertService.getAll();
+
+        List<AlertReportDTO> alertReports = alerts.stream()
+            .map(alert -> new AlertReportDTO(
+                alert.id(),
+                alert.sensorId(),
+                alert.sensorModel(),
+                alert.type().name(),
+                alert.timetamp().toString(),
+                alert.description(),
+                alert.status().name(),
+                String.join(", ", alert.photoUrls())
+            ))
             .toList();
 
-        Map<String, Object> data = Map.of("alerts", alerts);
+        Map<String, Object> data = new HashMap<>();
+        data.put("alerts", alertReports);
 
         try (InputStream is = templateLocation.getInputStream()) {
             byte[] reportBytes = JxlsPoiTemplateFillerBuilder.newInstance()
